9125210bc1aa8f2520618f2cf368b91185c2713c
Riss <rissgroupdev2@gmail.com>
2022-07-13 12:43:30 +0500

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,3 +122,19 @@ up.on('click', function() {
 
 });
 // partner section script end
+// tokenomics section javascript start
+$(function () {
+    $("dd").slideUp(1);
+    $("dt, dt .faq-toggle").click(function () {
+      var $this = $(this),$parent = $this.parent(),outer = true;
+      if ($this.is('.faq-toggle')) {$parent = $parent.parent();outer = false;}
+      if ($parent.hasClass('active')) {
+        $parent.removeClass('active').find('dd').slideUp(500);
+      } else {
+        $parent.siblings().removeClass('active').find('dd').slideUp(500);
+        $parent.addClass('active').find('dd').slideDown(500);
+      }
+      return outer;
+    });
+  });
+  // tokenomics section javascript end
