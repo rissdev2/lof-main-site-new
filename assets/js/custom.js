8ef5c26c5f7a39aba41ebb56fea15a08dd009297
rissdev2 <rissgroupdev2@gmail.com>
2022-07-19 17:46:15 +0500

    features section

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      margin: 50,
+      loop: true,
+      nav: false,
+    });
+  });
+  
