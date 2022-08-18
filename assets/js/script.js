e5847e07be992c8b0e4242dc4ee99e392ae72feb
rissdev2 <rissgroupdev2@gmail.com>
2022-08-18 21:19:43 +0500

    changes 18-8

--- assets/js/script.js
+++ assets/js/script.js
@@ -199,29 +199,18 @@ $(window).scroll(function() {
 
   var oTop = $('#counter').offset().top - window.innerHeight;
   if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 3000,
+    $('.count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 4000,
           easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
+          step: function (now) {
+              // $(this).text(Math.ceil(now));
+             $(this).text(Math.ceil(now).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
           }
-
-        });
-    });
+      });
+  });
     a = 1;
   }
 
